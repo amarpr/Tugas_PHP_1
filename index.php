322538a9f46f09aea4f33663dafd45297e3da107
Muhamar <amararrasyid@gmail.com>
2020-05-30 20:39:19 +0700

    Tugas 1 PHP
    Mengerjakan Tugas Niomic

---
+++ index.php
@@ -0,0 +1,229 @@
+<?php
+$harga1 = 13000;
+$harga2 = 15000;
+$harga3 = 1000;
+$harga4 = 3000;
+$harga5 = 5000;
+$harga6 = 4000;
+
+echo ("<h1>Daftar Menu</h1>");
+
+echo ("
+<table>
+<tr>
+    <td>
+        <ul>
+            <li>
+                Ayam Goreng
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga1
+            </li>
+        </ul>
+    </td>
+</tr>
+<tr>
+    <td>
+        <ul>
+            <li>
+                Ayam Bakar
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga2
+            </li>
+        </ul>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        <ul>
+            <li>
+                Ayam Sayur
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga1
+            </li>
+        </ul>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        <ul>
+            <li>
+                Tempe Goreng
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga3
+            </li>
+        </ul>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        <ul>
+            <li>
+                Tahu Goreng
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga3
+            </li>
+        </ul>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        <ul>
+            <li>
+                Soto
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga4
+            </li>
+        </ul>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        <ul>
+            <li>
+                Nasi putih
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga5
+            </li>
+        </ul>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        <ul>
+            <li>
+                Es Teh
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga6
+            </li>
+        </ul>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        <ul>
+            <li>
+                Es Tebu
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                =
+            </li>
+        </ul>
+    </td>
+    <td>
+        <ul style='list-style-type: none;'>
+            <li>
+                Rp. $harga5
+            </li>
+        </ul>
+    </td>
+</tr>
+</table>
+        ");
+?>
---
+++ index2.php
@@ -0,0 +1,42 @@
+<?php
+$harga1 = 1000;
+$harga2 = 15000;
+$harga3 = 20000;
+$harga4 = 5000;
+$harga5 = 1000;
+
+
+echo ("<h1>DAFTAR MENU</h1>");
+
+echo ("
+    <table border='1'>
+        <tr>
+            <td>No</td>
+            <td>Nama Menu</td>
+            <td>Harga</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Es Duren</td>
+            <td>Rp. $harga2</td>
+        </tr>
+            <tr>
+            <td>3</td>
+            <td>Es Millo</td>
+            <td>Rp. $harga3</td>
+        </tr>
+        </tr>
+            <tr>
+            <td>4</td>
+            <td>Es Teh</td>
+            <td>Rp. $harga4</td>
+        </tr>
+        </tr>
+            <tr>
+            <td>5</td>
+            <td>Piscok</td>
+            <td>Rp. $harga5</td>
+        </tr>
+    </table>
+");
+?>
